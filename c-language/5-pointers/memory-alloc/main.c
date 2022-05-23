7585b6e7fa612c388633bacab6115bec87ca6350
gdserafini <gdinizserafini@gmail.com>
2022-05-23 15:56:02 -0300

    memory allocation add

---
+++ c-language/5-pointers/memory-alloc/main.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+void choose(int op){
+    switch(op){
+        case 1:
+            mem_alloc1();
+            break;
+        case 2:
+            mem_alloc2();
+            break;
+        default:
+            printf("\nERROR\n");
+            exit(0);
+    }
+}
+
+void atrib_print1(int array[SIZE]){
+    printf("\n");
+
+    for(int i = 0; i < SIZE; i++){
+        array[i] = i;
+
+        printf("%d ", array[i]);
+    }
+
+    printf("\n\n");
+}
+
+void atrib_print2(int matrix[SIZE][SIZE]){
+    printf("\n");
+
+    for(int i = 0; i < SIZE; i++){
+        for(int j = 0; j < SIZE; j++){
+            matrix[i][j] = i + j;
+            
+            printf("%d ", matrix[i][j]);
+        }
+
+        printf("\n\n");
+    }
+}
+
+void mem_alloc1(){
+    int* array = (int *)malloc(sizeof(int) * SIZE);
+
+    verif_array(array);
+
+    free(array);
+}
+
+void verif_array(int array[SIZE]){
+    if(array = malloc(12)){
+        atrib_print1(array);
+    }
+    else{
+        printf("\nERROR\n");
+        exit(0);
+    }
+}
+
+void mem_alloc2(){
+    int** matrix = (int **)malloc(sizeof(int*) * SIZE);
+
+    for(int i = 0; i < SIZE; i++){
+        matrix[i] = (int *)malloc(sizeof(int) * SIZE);
+    }
+
+    if(matrix[0] = malloc(12)){
+        atrib_print2(*matrix);
+    }
+    else{
+        printf("\nERRO\n");
+        exit(0);
+    }
+
+    for(int i = 0; i < SIZE; i++){
+        free(matrix[i]);
+    }
+
+    free(matrix);
+}
+
+void enter_option(int* op){
+    printf("\n1 for allocate the array, 2 for allocate the matrix: ");
+    scanf("%d", op);
+}
+
+void print_intro(){
+    printf("\n**Memory allocation**\n");
+}
+
+int main(){
+    int option;
+
+    print_intro();
+    enter_option(&option);
+    choose(option);
+
+    return 0;
+}
---
+++ c-language/5-pointers/memory-alloc/main.h
@@ -0,0 +1,10 @@
+#define SIZE 3
+
+void choose(int op);
+void atrib_print1(int array[SIZE]);
+void atrib_print2(int matrix[SIZE][SIZE]);
+void mem_alloc1();
+void verif_array(int array[SIZE]);
+void mem_alloc2();
+void enter_option(int* op);
+void print_intro();
