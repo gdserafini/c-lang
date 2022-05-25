2a8a3555e182ac467c0fbdc78d2a53114006795c
gdserafini <gdinizserafini@gmail.com>
2022-05-25 12:40:56 -0300

    8-read-strings actualized

--- c-language/8-i-o-console/read-string/main.c
+++ c-language/8-i-o-console/read-string/main.c
@@ -1,16 +1,39 @@
+//algoritm -> enter char by char = string
+
 #include <stdio.h>
 
-int main(){
-    char string1[10], string2[10], ch;
+//**PROTOTYPE**
+void enter_print_strings(void);
+void read_string_char(void);
+int getc(FILE *stream);
+
+int main(void){
+    enter_print_strings();
+    read_string_char();
+
+    return 0;
+}
+
+//**FUNCTIONS**
+
+//**read string -> print
+void enter_print_strings(){
+    char string1[10], string2[10];
 
     gets(string1);
     printf("%s", string1);
 
     scanf("%s", string2);
     printf("%s", string2);
+}
 
-    ch = getchar();
-    printf("%c", ch);
+//**??enter char by char -> put on the string??
+void read_string_char(){
+    char string3[10];
 
-    return 0;
+    for(int i = 0; i < 10; i++){
+        string3[i] = getc(stdin);
+    }
+
+    printf("%s", string3);
 }
