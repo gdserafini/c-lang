48885640dd2079b21efbbb3007d39a217b262124
gdserafini <gdinizserafini@gmail.com>
2022-05-27 12:33:07 -0300

    12-stdio-format finished

---
+++ c-language/12-i-o-functions/stdio-format/main.c
@@ -0,0 +1,111 @@
+/* CREATE A .TXT FILE AND WRITE A FORMATTED TEXT */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STR_LEN 100
+#define END_ENTER '\n'
+
+void enter_file_name(char *name_enter);
+void create_file(char *name_create);
+void console_write_file();
+int console_end_file(char char_end);
+void format_text_file(int *count_format);
+void verif_file_name(char *name_verif);
+
+FILE* f;
+
+/* BASIC ALGORITM */
+int main(){
+    char file_name[STR_LEN];
+
+    enter_file_name(file_name);
+    create_file(file_name);
+    console_write_file();
+    fclose(f);
+
+    return 0;
+}
+
+/* FUCTIONS */
+
+/* TYPE A FILE NAME AND CHECK IF IT IS A .TXT FILE */
+void enter_file_name(char *name_enter){
+    printf("\nEnter the file name: ");
+    fgets(name_enter, STR_LEN, stdin);
+
+    verif_file_name(name_enter);
+}
+
+/* CREATE A .TXT FILE */
+void create_file(char *name_create){
+    f = fopen(name_create, "w");
+
+    if(f == NULL || ferror(f)){
+        printf("\nFILE ERROR\n");
+        exit(0);
+    }
+}
+
+/* WRITE A FORMATTED TEXT - CHAR BY CHAR */
+void console_write_file(){
+    char char_write;
+    int count_char = 0;
+
+    do{
+        char_write = fgetc(stdin);
+        /* NOT PRINT \n */
+        if(char_write != END_ENTER){
+            fputc(char_write, f);
+        }
+
+        count_char++;
+
+        format_text_file(&count_char);
+
+        if(ferror(f)){
+            printf("\nWRITE ERROR\n");
+            exit(0);
+        }
+    }while(!console_end_file(char_write));
+
+    fseek(f, 0, SEEK_END);
+}
+
+/* ENTER -> END  */
+int console_end_file(char char_end){
+    if(char_end == END_ENTER){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+/* \n IF N° CHAR = 100 */
+void format_text_file(int *count_format){
+    if(*count_format == STR_LEN -1){
+        fputc(END_ENTER, f);
+        *count_format = 0;
+    }
+}
+
+/* != .TXT -> ERROR */
+void verif_file_name(char *name_verif){
+    int equal = 0, len = strlen(name_verif);
+
+    for(int i = 0; i < len - 3; i++){
+        if(name_verif[i] == '.' && 
+        name_verif[i+1] == 't' &&
+        name_verif[i+2] == 'x' &&
+        name_verif[i+3] == 't'){
+            equal = 1;
+        }
+    }
+
+    if(equal != 1){
+        printf("\nERROR - ENTER A .TXT FILE NAME\n");
+        exit(0);
+    }
+}
