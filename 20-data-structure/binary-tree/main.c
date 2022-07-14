57da3636d6ab706b376c9a31b58638d7ddc8b175
gdserafini <gdinizserafini@gmail.com>
2022-07-13 22:17:34 -0300

    binary tree add

--- 20-data-structure/binary-tree/main.c
+++ 20-data-structure/binary-tree/main.c
@@ -1,48 +1,92 @@
 /* BINARY TREE 
-EXEMPLE - ORDERED 10 NUMBERS */
+exemple - 10 numbers - ordered binary tree 
+and binary search*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define TSIZE 15
-#define NMAX 100
+#define TSIZE 10 //tree size
+#define NMAX 100 //n° max in rand()
 
-struct numbers{
-    int num;
-    struct numbers *right;
-    struct numbers *left;
-};
+typedef struct node{
+    int data; 
 
-void init_tree(struct numbers *n);
-void stree(struct numbers *n);
-void print_address(struct numbers *n);
+    struct node *right; 
+    struct node *left;
+} NODE;
 
-int key = TSIZE/2;
-
-int main(void){
-    struct numbers n[TSIZE];
-
-    /* MAIN ALGORITM */
-    srand(time(0));
-    init_tree(n);
-    stree(n);
-    print_address(n);
-
-    return 0;
-}
-
-void init_tree(struct numbers *n){
+/* place a aleatory number in each array position */
+void init_array(int *array){
     for(int i = 0; i < TSIZE; i++){
-        n[i].right = NULL;
-        n[i].left = NULL;
+        array[i] = rand() % NMAX;
     }
 }
 
-void stree(struct numbers *n){
-    
+NODE *insert(NODE *root, int data){
+    NODE *temp = (NODE*)malloc(sizeof(NODE));
+
+    temp->data = data; //enter a number and clean the pointers
+    temp->right = NULL;
+    temp->left = NULL;
+
+    if(root = NULL){
+        root = temp;
+    }
+    else{
+        NODE *current = root;
+        NODE *parent = NULL;
+
+        while(1){
+            parent = current;
+            
+            if(data < parent->data){
+                current = current->left;
+
+                if(current = NULL){
+                    parent->left = temp;
+                    return root;
+                }
+            }
+            else{
+                current = current->right;
+
+                if(current = NULL){
+                    parent->right = temp;
+                    return root;
+                }
+            }
+        }
+    }
+
+    return root; //return root pointer
 }
 
-void print_address(struct numbers *n){
-    
+void print_inorder(NODE *root){
+    if(root != NULL){
+        print_inorder(root->left);
+        printf("%d ", root->data);
+        print_inorder(root->right);
+    }
+}
+
+int main(void){
+    NODE n[TSIZE];
+    int array[TSIZE];
+
+    /* MAIN ALGORITM */
+    srand(time(0));
+    init_array(array);
+
+    //create a binary tree
+    int i;
+    NODE *root = NULL;
+
+    for(i = 0; i < TSIZE; i++){
+        root = insert(root, array[i]);
+    }
+
+    print_inorder(root);
+
+    return 0;
 }
